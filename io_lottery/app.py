c3b553c909c8453d5aee8bc75f48499a815bc51c
AnanasekE <janeksambor@gmail.com>
2023-01-31 15:11:29 +0100

    commit

--- io_lottery/app.py
+++ io_lottery/app.py
@@ -7,13 +7,33 @@ from io_lottery.controllers import AddUserController, AddUserRequest
 app = Flask(__name__)
 
 
-@app.post("/users")
-def add_user() -> Response:
-    controller = AddUserController()
-    controller.add(request=AddUserRequest(json=request.json))
-    return request.json
+# @app.post("/users")
+# def add_user() -> Response:
+#     controller = AddUserController()
+#     controller.add(request=AddUserRequest(json=request.json))
+#     return request.json
 
 
 @app.get("/users")
 def get_user() -> Response:
     return Response(status=501)
+
+
+@app.post("/users")
+def add_user() -> Response:
+    return Response(status=201)
+
+
+@app.put("/users/<int:user_id>")
+def update_user(user_id: int) -> Response:
+    return Response(status=200)
+
+
+@app.patch("/users/<int:user_id>")
+def patch_user(user_id: int) -> Response:
+    return Response(status=200)
+
+
+@app.delete("/users/<int:user_id>")
+def delete_user(user_id: int) -> Response:
+    return Response(status=204)
--- tests/unit/test_users.py
+++ tests/unit/test_users.py
@@ -1,7 +1,7 @@
 from factory import DictFactory
 from factory.fuzzy import FuzzyText, FuzzyInteger, FuzzyFloat
 
-from io_lottery.app import add_user, app, get_user
+from io_lottery.app import add_user, app, get_user, update_user, patch_user, delete_user
 
 
 class UserPayloadFactory(DictFactory):
@@ -31,3 +31,29 @@ def test_returns_sent_user_get() -> None:
 #     with app.test_request_context(path='/users', method='PATCH', json=payload):
 #         actual = patch_user()
 #         assert actual.status_code == 501
+
+def test_returns_sent_user_post() -> None:
+    payload = UserPayloadFactory()
+    with app.test_request_context(path='/users', method='POST', json=payload):
+        actual = add_user()
+        assert actual.status_code == 201
+
+
+def test_returns_send_user_put() -> None:
+    payload = UserPayloadFactory()
+    with app.test_request_context(path='/users', method='PUT', json=payload):
+        actual = update_user(1)
+        assert actual.status_code == 200
+
+
+def test_returns_sent_user_patch() -> None:
+    payload = UserPayloadFactory()
+    with app.test_request_context(path='/users', method='PATCH', json=payload):
+        actual = patch_user(1)
+        assert actual.status_code == 200
+
+# nie działa
+def test_returns_sent_user_delete() -> None:
+    with app.test_request_context(path='/users', method='DELETE'):
+        actual = delete_user(1)
+        assert actual.status_code == 204
