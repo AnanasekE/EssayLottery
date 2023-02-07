35982f14424379413e4d558ce897b19a81fe5293
AnanasekE <janeksambor@gmail.com>
2023-02-07 15:27:37 +0100

    commit

--- tests/unit/test_users.py
+++ tests/unit/test_users.py
@@ -4,6 +4,7 @@ from factory.fuzzy import FuzzyText, FuzzyInteger, FuzzyFloat
 # from io_lottery.app import add_user, app, get_user, update_user, patch_user, delete_user
 from io_lottery.app import app, add_user
 
+
 class UserPayloadFactory(DictFactory):
     name = FuzzyText()
     last_name = FuzzyText()
@@ -18,7 +19,6 @@ def test_returns_sent_user() -> None:
     with app.test_request_context(path="/users", method='POST', json=payload):
         actual = add_user()
     assert actual == payload
-
 
 # def test_returns_sent_user_get() -> None:
 #     with app.test_request_context(path='/users', method='GET'):
